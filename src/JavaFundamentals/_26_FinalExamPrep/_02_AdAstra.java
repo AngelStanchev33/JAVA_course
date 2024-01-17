cab658c28fa9498da351c1c1560bbc70b969d471
AngelStanchev <angelstanchev33@gmail.com>
2024-01-17 02:33:15 -0500

    commit New Task

--- src/JavaFundamentals/_26_FinalExamPrep/_02_AdAstra.java
+++ src/JavaFundamentals/_26_FinalExamPrep/_02_AdAstra.java
@@ -10,29 +10,23 @@ public class _02_AdAstra {
 
         String line = scanner.nextLine();
 
-        Map<String, String> expDateMap = new LinkedHashMap<>();
-        Map<String, Integer> caloriesMap = new LinkedHashMap<>();
         List<String> items = new LinkedList<>();
 
         String regex = "([#|\\|]{1})([A-Za-z\\s]+)\\1([0-9]{2}/[0-9]{2}/[0-9]{2})\\1([0-9]{1,5})\\1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(line);
 
-
-        int totalCalories = 0;
+        double totalCalories = 0;
         while (matcher.find()) {
             String food = matcher.group(2);
             String date = matcher.group(3);
-            int calories = Integer.parseInt(matcher.group(4));
-
-            expDateMap.putIfAbsent(food, date);
-            caloriesMap.putIfAbsent(food, calories);
+            double calories = Double.parseDouble(matcher.group(4));
 
             totalCalories += calories;
-            items.add(String.format("Item: %s, Best before: %s, Nutrition: %d%n", food, date, calories));
+            items.add(String.format("Item: %s, Best before: %s, Nutrition: %.0f%n", food, date, calories));
         }
 
-        System.out.printf("You have food to last you for: %d days!%n", totalCalories/2000);
+        System.out.printf("You have food to last you for: %.0f days!%n", Math.floor(totalCalories / 2000));
 
         items.forEach(System.out::print);
 
