1d43e886a56461f83f064492f50f403b291f613f
AngelStanchev <angelstanchev33@gmail.com>
2024-03-12 22:26:42 -0400

    commit New Task

---
+++ src/_03_JavaAdvanced/_01_StacksandQueues/_04_MatchingBrackets.java
@@ -0,0 +1,25 @@
+package _03_JavaAdvanced._01_StacksandQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _04_MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String expression = scanner.nextLine();
+
+        ArrayDeque<Integer> indexesOfOpeningBrackets = new ArrayDeque<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char symbol = expression.charAt(i);
+
+            if (symbol == '(') {
+                indexesOfOpeningBrackets.push(i);
+            } else if (symbol == ')') {
+                System.out.println(expression.substring(indexesOfOpeningBrackets.pop(), i + 1));
+            }
+
+        }
+    }
+}
