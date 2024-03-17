8165019b972421337ef6849e547c3356841216d0
AngelStanchev <angelstanchev33@gmail.com>
2024-03-17 02:18:43 -0400

    commit New Task

--- src/_03_JavaAdvanced/_02_ExercisesStacksandQueues/_04_BasicQueueOperations.java
+++ src/_03_JavaAdvanced/_02_ExercisesStacksandQueues/_04_BasicQueueOperations.java
@@ -6,32 +6,34 @@ public class _04_BasicQueueOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tokens = scanner.nextLine().split("\\s+");
+        int[] tokens = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-        int n = Integer.parseInt(tokens[0]);// number of elements to enqueue
-        int s = Integer.parseInt(tokens[1]);//number of elements to dequeue
-        int x = Integer.parseInt(tokens[2]);// element that you should check whether is present in the queue
+        Deque<Integer> numStack = new ArrayDeque<>();
 
-        Deque<Integer> queue = new ArrayDeque<>();
+        int n = tokens[0];
+        int s = tokens[1];
+        int x = tokens[2];
 
-        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+        int[] numbs = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
         for (int i = 0; i < n; i++) {
-            queue.offer(numbers[i]);
+            numStack.offer(numbs[i]);
         }
-
         for (int i = 0; i < s; i++) {
-            queue.poll();
+            if (!numStack.isEmpty()) {
+                numStack.poll();
+            }
         }
-
-        if (queue.isEmpty()) {
+        if (numStack.isEmpty()) {
             System.out.println(0);
-        } else if (queue.contains(x)) {
+        } else if (numStack.contains(x)) {
             System.out.println(true);
         } else {
-            System.out.println(Collections.min(queue));
+            System.out.println(Collections.min(numStack));
         }
     }
 }
